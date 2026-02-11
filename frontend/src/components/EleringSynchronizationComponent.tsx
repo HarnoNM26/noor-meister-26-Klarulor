7e1196b48ca3dddcd6397a6ae4c92a4f95d849c8
Klarulor <klarulor@gmail.com>
2026-02-11 17:11:42 +0200

    Some changes to give more context when some exceptions from elering come

--- frontend/src/components/EleringSynchronizationComponent.tsx
+++ frontend/src/components/EleringSynchronizationComponent.tsx
@@ -66,7 +66,7 @@ export function EleringSynchronizationComponent(){
       <path
         d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"
       ></path></svg>
-    Sync prices</button>
+    Sync prices {isWorking ? "⬤" : ""}</button>
         </div>
     </div>
 }
