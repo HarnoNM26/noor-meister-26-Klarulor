557498ba647b66ef280d4f536143588f55264db4
Klarulor <klarulor@gmail.com>
2026-02-12 16:50:25 +0200

    end

--- backend/test.ts
+++ backend/test.ts
@@ -1,22 +1,22 @@
 import * as axios from 'axios';
  const start = "2026-01-11T12:05:19.502Z";
-// const end = "2026-03-11T12:06:28.502Z";
-// const location = "EE";
-// const base_url = "https://dashboard.elering.ee";
+const end = Date.now();
+const location = "EE";
+const base_url = "https://dashboard.elering.ee";
 
 
 
 // // EleringService.getPriceRanges(start,end,"ee").then(console.log);
 
 
-axios.post("http://127.0.0.1:3000/api/import/json", 
-    [{
-        timestamp: start,
-        location: "ee",
-        price_eur_mwh: 7770
-    }]
-).then(x => console.log(x.data));
+// axios.post("http://127.0.0.1:3000/api/import/json", 
+//     [{
+//         timestamp: start,
+//         location: "ee",
+//         price_eur_mwh: 7770
+//     }]
+// ).then(x => console.log(x.data));
 
 
 
-// axios.get(`http://127.0.0.1:3000/api/readings?start=${start}&end=${end}&location=${location}`).then(x => console.log(x.data)).catch(x => console.log(x, `Code: ${x.status}`, `Data: ${x.response?.data}`));
+axios.get(`http://127.0.0.1:3000/api/insights/prices?start=${start}&end=${end}&location=${location}`).then(x => console.log(x.data)).catch(x => console.log(x, `Code: ${x.status}`, `Data: ${x.response?.data}`));
